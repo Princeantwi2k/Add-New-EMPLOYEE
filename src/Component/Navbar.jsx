e1a32cbcf9f22779d5a33bcde022f7000b6791fa
prince antwi <antwi2k@gmail.com>
2021-07-22 15:14:30 +0800

    Added Navbar.jsx

---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav, NavLink } from "react-bootstrap";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <h3>Employee Account Manger</h3>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
